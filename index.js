d0a46cdbc0650b34e69039e79fb2e4910c433643
CUPAXX TECH <56408053+CUPAXX@users.noreply.github.com>
2025-02-25 14:42:39 +0700

    add database connection helpers

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-require("dotenv").config();
 const { PORT } = process.env;
 
+//create backend server
 const app = express();
 const server = require("http").createServer(app);
 
+//apply body-parser module
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//apply cord rules for frontend integration
 app.use(cors());
 
+//listen output server
 server.listen(PORT || 8000, () => {
   console.log(`Backend running on port ${PORT || 8000}`);
 });
